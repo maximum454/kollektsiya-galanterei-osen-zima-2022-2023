a55b9795fb3b186d1dcb04b4dc0142326c37812a
Maxim Karachyov <karachyov.m@gmail.com>
2022-08-26 15:36:03 +0300

    osen-zima-detail.scss adap

--- src/js/kollektsiya-galanterei-osen-zima-2022-2023.js
+++ src/js/kollektsiya-galanterei-osen-zima-2022-2023.js
@@ -2,4 +2,17 @@
 const swiperDetail = new Swiper(".swiper-detail", {
     slidesPerView: 1,
     spaceBetween: 0,
+    navigation: {
+        nextEl: '.swiper-button-next',
+        prevEl: '.swiper-button-prev',
+    },
+});
+
+const swiperPhoto = new Swiper(".swiper-photo", {
+    slidesPerView: 1,
+    spaceBetween: 0,
+    navigation: {
+        nextEl: '.swiper-button-next',
+        prevEl: '.swiper-button-prev',
+    },
 });
