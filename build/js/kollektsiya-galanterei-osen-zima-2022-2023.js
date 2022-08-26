1f6ebcf8ff75606a7e1bff2f998cc770655bb3b7
Maxim Karachyov <karachyov.m@gmail.com>
2022-08-26 10:06:19 +0300

    detail

---
+++ build/js/kollektsiya-galanterei-osen-zima-2022-2023.js
@@ -0,0 +1,9 @@
+const swiperSmall = new Swiper(".swiper-small", {
+    slidesPerView: 3,
+    spaceBetween: 22,
+});
+
+const swiperDetail = new Swiper(".swiper-detail", {
+    slidesPerView: 1,
+    spaceBetween: 0,
+});
